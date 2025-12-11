bd64a15cc48174c3557796e9be064d5ff32f1d98
Niffler77 <99284934+Niffler77@users.noreply.github.com>
2025-12-11 08:04:04 +0100

    apis zeugs

---
+++ APITest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+
+namespace APITest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Uri uri = new Uri("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
